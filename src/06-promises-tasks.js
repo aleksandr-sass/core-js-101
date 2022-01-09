bfe6ac66ad50656b556d666a3de89c9c2e728adc
aleksandr-sass <a.sass@tut.by>
2022-01-09 12:28:44 +0300

    feat: add solution for function 'getFastestPrimise'

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -85,8 +85,8 @@ function processAllPromises(array) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
